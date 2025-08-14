4ce8013c5509ae5a08b5bd67ec8ad63066cdbd9e
Nithin Kumar Jammichedu Kuruva <insane414425@gmail.com>
2025-08-14 01:12:36 +0000

    fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import "./styles.css";
+import { useEffect } from "react";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+      const data = await res.json();
+      setUsers(data.filter((user) => user.id % 2 === 0));
+      setLoading(!loading);
+    }
+
+    fetchData();
+  }, []);
+
+  if (loading) return <p> Loading Tasks....</p>;
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Fetch Users Data</h1>
+      {users.map((user) => (
+        <p key={user.id}>{user.name}</p>
+      ))}
     </div>
   );
 }
